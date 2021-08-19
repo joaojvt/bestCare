8d8a1d50a53820f4a48219be67f69385af1cfb87
João Vitor da Silva <joao.jvts@gmail.com>
2021-08-19 13:44:56 -0300

    corrigido deploy

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Routes from './Routes'
 function App() {
 
   return (
-    <Router basename={process.env.PUBLIC_URL} >
+    <Router basename={'/bestCare/'} >
       <div id="App">
         <Header/>
         <Routes/>
