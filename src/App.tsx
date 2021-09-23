2ffed3ca2b0dccfcbfffa16f66d26bc9d80177e2
joao.vitor <joao.vitor@iesde.com.br>
2021-09-23 17:57:05 -0300

    ajustado vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import './App.scss'
+
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Routes from './Routes'
